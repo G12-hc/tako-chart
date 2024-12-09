afc5de0146682f99148148a485a34ce3e6f418e0
Celeste <190388672+celie2@users.noreply.github.com>
2024-12-09 11:55:14 +0000

    Add Auto Uvicorn Server Reload
    Co-authored-by: Nazarii Arshynskyi <na@ggr.group>

--- app/main.py
+++ app/main.py
@@ -5,16 +5,20 @@ from fastapi.staticfiles import StaticFiles
 from app import routers
 from app.db.connection import initialize_db
 
-# Create the API application instance
-api_app = FastAPI(title="API app")
-api_app.include_router(routers.router)
 
-# Create the main application instance
-app = FastAPI(title="main app")
-app.mount("/api", api_app)
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+def create_app():
+    initialize_db()
+    # Create the API application instance
+    api_app = FastAPI(title="API app")
+    api_app.include_router(routers.router)
+
+    # Create the main application instance
+    app = FastAPI(title="main app")
+    app.mount("/api", api_app)
+    app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+
+    return app
+
 
 if __name__ == "__main__":
-    initialize_db()  # Initialize database or connections
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
+    uvicorn.run("app.main:create_app", host="127.0.0.1", port=8000, reload=True)
