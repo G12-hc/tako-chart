525695e4616c9a41ec83d495895896b4761ac218
Celeste <190388672+celie2@users.noreply.github.com>
2024-12-10 13:37:38 +0000

    chore(fe): format files with Prettier
    https://prettier.io/

--- frontend/scripts/javascript.js
+++ frontend/scripts/javascript.js
@@ -11,8 +11,8 @@ async function fetchData(endpoint) {
 }
 
 function drawTables(element, data) {
-  const mostCommitsTable = element.querySelector('.most-table');
-  const leastCommitsTable = element.querySelector('.least-table');
+  const mostCommitsTable = element.querySelector(".most-table");
+  const leastCommitsTable = element.querySelector(".least-table");
 
   const highestCommitters = data.slice(0, 3);
   const lowestCommitters = data.slice(Math.max(data.length - 3, 0)).reverse();
@@ -20,15 +20,15 @@ function drawTables(element, data) {
   var i = 0;
 
   for (const [author, commitCount] of highestCommitters) {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     mostCommitsTable.appendChild(row);
-    const rankCell = document.createElement('td');
-    rankCell.class = 'td';
+    const rankCell = document.createElement("td");
+    rankCell.class = "td";
     rankCell.textContent = `${i + 1}`;
     row.appendChild(rankCell);
-    const authorCell = document.createElement('td');
-    authorCell.class = 'td';
-    authorCell.textContent = `${author} (${commitCount} commits)`
+    const authorCell = document.createElement("td");
+    authorCell.class = "td";
+    authorCell.textContent = `${author} (${commitCount} commits)`;
     row.appendChild(authorCell);
 
     i++;
@@ -37,22 +37,22 @@ function drawTables(element, data) {
   i = data.length;
 
   for (const [author, commitCount] of lowestCommitters) {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     leastCommitsTable.appendChild(row);
-    const rankCell = document.createElement('td');
-    rankCell.class = 'td';
+    const rankCell = document.createElement("td");
+    rankCell.class = "td";
     rankCell.textContent = `${i}`;
     row.appendChild(rankCell);
-    const authorCell = document.createElement('td');
-    authorCell.class = 'td';
-    authorCell.textContent = `${author} (${commitCount} commits)`
+    const authorCell = document.createElement("td");
+    authorCell.class = "td";
+    authorCell.textContent = `${author} (${commitCount} commits)`;
     row.appendChild(authorCell);
 
     i--;
   }
 }
 
-async function drawHistogram(domElement,{xLabel, yLabel, endpoint, getX}) {
+async function drawHistogram(domElement, { xLabel, yLabel, endpoint, getX }) {
   const data = await fetchData(endpoint);
 
   // Set the plotly settings, data, and other things, based on the chart type
@@ -62,34 +62,33 @@ async function drawHistogram(domElement,{xLabel, yLabel, endpoint, getX}) {
   plotlyData = [
     {
       x: data.map(getX),
-      type: 'histogram',
-      marker: {color: 'rgba(5,112,1,0.65)'},
-      }];
+      type: "histogram",
+      marker: { color: "rgba(5,112,1,0.65)" },
+    },
+  ];
 
   layout = {
-    xaxis: {title: {text: xLabel}},
-    yaxis: {title: {text: yLabel}},
-    };
+    xaxis: { title: { text: xLabel } },
+    yaxis: { title: { text: yLabel } },
+  };
 
-  Plotly.newPlot(domElement.querySelector('.plotly-graph'), plotlyData,layout);
-
-  
+  Plotly.newPlot(domElement.querySelector(".plotly-graph"), plotlyData, layout);
 }
 
-async function drawPieChart(domElement, {endpoint, getLabel, getValue}) {
+async function drawPieChart(domElement, { endpoint, getLabel, getValue }) {
   const data = await fetchData(endpoint);
 
   const plotlyData = [
-  {
-    labels: data.map(getLabel),
-    values: data.map(getValue),
-    type: 'pie',
-    //maybe for loop for the firs 3??
-    textinfo: "none",
-    showlegend: false,
-    hoverinfo: 'percent+label',
-    domain: {'x': [0, 1], 'y': [0, 1]},
-  }
+    {
+      labels: data.map(getLabel),
+      values: data.map(getValue),
+      type: "pie",
+      //maybe for loop for the firs 3??
+      textinfo: "none",
+      showlegend: false,
+      hoverinfo: "percent+label",
+      domain: { x: [0, 1], y: [0, 1] },
+    },
   ];
 
   const layout = {
@@ -100,57 +99,67 @@ async function drawPieChart(domElement, {endpoint, getLabel, getValue}) {
       r: 50,
       t: 25,
       b: 25,
-      pad: 2
-    }
-  }
+      pad: 2,
+    },
+  };
   // `dataAsArrays` is just `data` but with each row as an array instead of an object,
   // to allow sharing the "most" and "least" table code below
   const dataAsArrays = data.map((row) => [getLabel(row), getValue(row)]);
 
-  Plotly.newPlot(domElement.querySelector('.plotly-graph'), plotlyData, layout);
+  Plotly.newPlot(domElement.querySelector(".plotly-graph"), plotlyData, layout);
   drawTables(domElement, dataAsArrays);
 }
 
-async function drawBarChart(domElement, {xLabel, yLabel, endpoint, getX, getY}) {
+async function drawBarChart(
+  domElement,
+  { xLabel, yLabel, endpoint, getX, getY },
+) {
   const data = await fetchData(endpoint);
 
-const       plotlyData = [
-        {
-          x: data.map(getX),
-          y: data.map(getY),
-          marker: {color: 'rgba(5,112,1,0.65)'},
-          textinfo: 'none'
-        }
-      ];
-      layout = {
-        xaxis: {title: {text: xLabel}},
-        yaxis: {title: {text: yLabel}},
-        barcornerradius: 7,
-
-      };
+  const plotlyData = [
+    {
+      x: data.map(getX),
+      y: data.map(getY),
+      marker: { color: "rgba(5,112,1,0.65)" },
+      textinfo: "none",
+    },
+  ];
+  layout = {
+    xaxis: { title: { text: xLabel } },
+    yaxis: { title: { text: yLabel } },
+    barcornerradius: 7,
+  };
 
   // `dataAsArrays` is just `data` but with each row as an array instead of an object,
   // to allow sharing the "most" and "least" table code below
-  const dataAsArrays = data.map((row) => [getX(row),getY(row)]);
+  const dataAsArrays = data.map((row) => [getX(row), getY(row)]);
 
-  Plotly.newPlot(domElement.querySelector('.plotly-graph'), plotlyData, layout);
+  Plotly.newPlot(domElement.querySelector(".plotly-graph"), plotlyData, layout);
   drawTables(domElement, dataAsArrays);
 }
 
-drawPieChart(document.querySelector('.commits-per-author-container'), {getLabel(row) { return row.author }, getValue(row) { return row.commit_count }, endpoint: 'commits-per-author' });
-drawBarChart(document.querySelector('.code-lines-in-files-per-project-container'), {
-  xLabel: 'File',
-  yLabel: 'Lines of code',
-  getX: (row) => row.path,
-  getY: (row) => row.line_count,
-  endpoint: 'line-counts-per-file',
+drawPieChart(document.querySelector(".commits-per-author-container"), {
+  getLabel(row) {
+    return row.author;
+  },
+  getValue(row) {
+    return row.commit_count;
+  },
+  endpoint: "commits-per-author",
 });
-drawHistogram(
-  document.querySelector('.commits-over-time-container'),
+drawBarChart(
+  document.querySelector(".code-lines-in-files-per-project-container"),
   {
-    getX: (commit) => commit.date,
-    xLabel: 'Date',
-    yLabel: 'Commit count',
-    endpoint: 'commit-dates',
-  }
+    xLabel: "File",
+    yLabel: "Lines of code",
+    getX: (row) => row.path,
+    getY: (row) => row.line_count,
+    endpoint: "line-counts-per-file",
+  },
 );
+drawHistogram(document.querySelector(".commits-over-time-container"), {
+  getX: (commit) => commit.date,
+  xLabel: "Date",
+  yLabel: "Commit count",
+  endpoint: "commit-dates",
+});
