7bc74d0c1bde3c0bf3949a7ec3c30692951ef576
crazybrain23 <44417085+crazybrain23@users.noreply.github.com>
2024-12-13 12:02:59 +0000

    (wip) Start repo management javascript

--- frontend/scripts/repoManagement.js
+++ frontend/scripts/repoManagement.js
@@ -1,17 +1,28 @@
-
-async function initReposTable() {
+async function initReposTable(element) {
     // Fetch data from the repository
     const data = await fetch("/api/repos");
 
     if (!data.ok) {
         // TODO: error
+        return;
     }
 
     const repos = JSON.parse(await data.text()).repos;
-
+    // const dataAsArrays = repos.map();
 
     // Put data into table
-    const table = element.querySelector(".repo-table");
+    const managementTable = element.querySelector(".management-table");
+    console.log(dataAsArrays);
+    for (const { name, owner, linkedAt, modifiedAt } of repos) {
+        const row = document.createElement("tr");
+        managementTable.appendChild(row);
+
+        const repoCell = document.createElement("td");
+        // const repoCellLink = document.createElement("a","href")
+        repoCell.textContent = `${owner}/${name}`;
+
+
+    }
 
 }
 
